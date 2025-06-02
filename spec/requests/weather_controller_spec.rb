3d84e71dd180be46690c30b083546a7188978d04
Dmitry Jum <xfiler@ymail.com>
2025-06-02 09:00:25 -0700

    adds weather controller test

---
+++ spec/requests/weather_controller_spec.rb
@@ -0,0 +1,50 @@
+require "rails_helper"
+
+RSpec.describe "WeatherController", type: :request do
+  let(:address) { "New York, NY" }
+  let(:fetcher_result) do
+    {
+      city: address,
+      current: { temp: 70, weekday: "Monday", description: "Sunny", humidity: 60, wind: "5 mph" },
+      daily: [
+        { name: "Mon", description: "Sunny", temp: 70 },
+        { name: "Tue", description: "Cloudy", temp: 65 }
+      ]
+    }
+  end
+
+  before do
+    Rails.cache.clear
+  end
+
+  it "returns a 200 response and assigns forecasts for a valid address" do
+    allow(WeatherFetcher).to receive(:new).with(anything).and_return(double(call: fetcher_result))
+
+    get weather_search_path, params: { address: address }
+
+    expect(response).to have_http_status(:ok)
+    expect(assigns(:current_forecast)).to eq(fetcher_result[:current])
+    expect(assigns(:daily_forecast)).to eq(fetcher_result[:daily])
+    expect(assigns(:from_cache)).to be_falsey
+    expect(assigns(:error)).to be_nil
+  end
+
+  it "reads result from cache if present" do
+    Rails.cache.write("weather-fetcher-#{address.parameterize}", fetcher_result, expires_in: 30.minutes)
+
+    get weather_search_path, params: { address: address }
+
+    expect(assigns(:current_forecast)).to eq(fetcher_result[:current])
+    expect(assigns(:from_cache)).to be_truthy
+  end
+
+  it "assigns error if WeatherFetcher returns error" do
+    error_result = { error: "Could not geocode address" }
+    allow(WeatherFetcher).to receive(:new).with(anything).and_return(double(call: error_result))
+
+    get weather_search_path, params: { address: address }
+
+    expect(assigns(:current_forecast)).to be_nil
+    expect(assigns(:error)).to eq("Could not geocode address")
+  end
+end
