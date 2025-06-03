2de19fc45a05910e2439951e7c24f03766de45e4
Dmitry Jum <xfiler@ymail.com>
2025-06-02 22:42:08 -0700

    converts every temperature and validates for special characters

--- app/javascript/controllers/temperature_controller.js
+++ app/javascript/controllers/temperature_controller.js
@@ -2,36 +2,44 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["value", "f", "c"]
-  static values = { temperature: Number }
-  currentUnit = "F"
+  initialize() {
+    this.originalTemps = {};
+    this.currentUnit = "F";
+  }
 
   connect() {
-    this.originalTemp = Number(this.valueTarget.textContent)
-    this.showF()
+    this.valueTargets.forEach(element => {
+      this.originalTemps[element.id] = Number(element.attributes["data-original"].value);
+    });
+    this.showF();
   }
 
   toC(event) {
-    event.preventDefault()
-    if (this.currentUnit === "C") return
-    const c = Math.round((this.originalTemp - 32) * 5 / 9)
-    this.valueTarget.textContent = c
-    this.currentUnit = "C"
-    this.cTarget.classList.add("underline")
-    this.fTarget.classList.remove("underline")
+    event.preventDefault();
+    if (this.currentUnit === "C") return;
+    this.valueTargets.forEach(element => {
+      const original = this.originalTemps[element.id];
+      const celsius = Math.round((original - 32) * 5 / 9);
+      element.querySelector("span").textContent = celsius;
+    });
+    this.currentUnit = "C";
+    this.cTarget.classList.add("underline");
+    this.fTarget.classList.remove("underline");
   }
 
   toF(event) {
-    event.preventDefault()
-    if (this.currentUnit === "F") return
-    this.valueTarget.textContent = this.originalTemp
-    this.currentUnit = "F"
-    this.fTarget.classList.add("underline")
-    this.cTarget.classList.remove("underline")
+    event.preventDefault();
+    if (this.currentUnit === "F") return;
+    this.valueTargets.forEach(element => {
+      element.querySelector("span").textContent = this.originalTemps[element.id];
+    });
+    this.currentUnit = "F";
+    this.fTarget.classList.add("underline");
+    this.cTarget.classList.remove("underline");
   }
 
   showF() {
-    this.valueTarget.textContent = this.originalTemp
-    this.fTarget.classList.add("underline")
-    this.cTarget.classList.remove("underline")
+    this.fTarget.classList.add("underline");
+    this.cTarget.classList.remove("underline");
   }
 }
