e3a177c9cfa77479ca4afecdfca24159c17524cc
Dmitry Jum <xfiler@ymail.com>
2025-06-04 22:02:20 -0700

    removes cls

--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -5,20 +5,13 @@ export default class extends Controller {
 
   connect() {
     document.addEventListener("turbo:before-fetch-request", this.show.bind(this))
-    document.addEventListener("turbo:frame-load", this.hide.bind(this))
   }
 
   disconnect() {
     document.removeEventListener("turbo:before-fetch-request", this.show.bind(this))
-    document.removeEventListener("turbo:frame-load", this.hide.bind(this))
   }
 
   show() {
-    this.spinnerTarget.classList.remove("hidden")
-    this.forecastTarget.innerHTML = ""
-  }
-
-  hide() {
-    this.spinnerTarget.classList.add("hidden")
+    this.forecastTarget.innerHTML = this.spinnerTarget.innerHTML
   }
 }
